53777755ea1738555ca21bde7f1d115b6f09a22d
Gerald Buttinger <gerald@code.florist>
2021-12-23 00:32:39 +0100

    ADD esm module file

---
+++ dist/privacy-policy-text.esm.js
@@ -0,0 +1,16 @@
+export * as colonPrefixSingularDe from './json/colon-prefix/singular/de.json'
+export * as colonPrefixSingularEn from './json/colon-prefix/singular/en.json'
+export * as colonPrefixPluralDe from './json/colon-prefix/plural/de.json'
+export * as colonPrefixPluralEn from './json/colon-prefix/plural/en.json'
+
+export * as curlyWrapSingularDe from './json/curly-wrap/singular/de.json'
+export * as curlyWrapSingularEn from './json/curly-wrap/singular/en.json'
+export * as curlyWrapPluralDe from './json/curly-wrap/plural/de.json'
+export * as curlyWrapPluralEn from './json/curly-wrap/plural/en.json'
+
+export * as doubleCurlyWrapSingularDe from './json/double-curly-wrap/singular/de.json'
+export * as doubleCurlyWrapSingularEn from './json/double-curly-wrap/singular/en.json'
+export * as doubleCurlyWrapPluralDe from './json/double-curly-wrap/plural/de.json'
+export * as doubleCurlyWrapPluralEn from './json/double-curly-wrap/plural/en.json'
+
+export * as providers from './json/providers.json'
