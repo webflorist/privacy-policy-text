8194ac60001bacf1ae6deb843b86fb15d14fc67c
Gerald Buttinger <gerald@code.florist>
2022-01-02 02:31:31 +0100

    ADD additional processors and privacy shield links

--- src/processors.php
+++ src/processors.php
@@ -10,6 +10,8 @@ return [
 		'name' => 'Google LLC',
 		'address' => '1600 Amphitheatre Parkway, Mountain View, CA 94043, USA',
 		'privacy_policy' => 'https://policies.google.com/privacy',
+		'privacy_shield' =>
+			'https://www.privacyshield.gov/participant?id=a2zt000000001L5AAI',
 	],
 	'twilio_eu' => [
 		'name' => 'Twilio Ireland Limited',
@@ -20,20 +22,43 @@ return [
 		'name' => 'Twilio Inc.',
 		'address' => '375 Beale Street, Suite 300, San Francisco, CA 94105, USA',
 		'privacy_policy' => 'https://www.twilio.com/legal/privacy',
+		'privacy_shield' =>
+			'https://www.privacyshield.gov/participant?id=a2zt0000000TNLbAAO',
 	],
 	'netlify' => [
 		'name' => 'Netlify Inc.',
 		'address' => '2325 3rd Street, Suite 296, San Francisco, CA 94107, USA',
 		'privacy_policy' => 'https://www.netlify.com/privacy/',
+		'privacy_shield' =>
+			'https://www.privacyshield.gov/participant?id=a2zt00000008RhTAAU',
 	],
 	'stripe' => [
 		'name' => 'Stripe Inc.',
 		'address' => '185 Berry Street, Suite 550, San Francisco, CA 94107, USA',
 		'privacy_policy' => 'https://stripe.com/privacy',
+		'privacy_shield' =>
+			'https://www.privacyshield.gov/participant?id=a2zt0000000TQOUAA4',
 	],
 	'storyblok' => [
 		'name' => 'Storyblok GmbH',
 		'address' => 'Peter-Behrens-Platz 2, A-4020 Linz, Austria, EU',
 		'privacy_policy' => 'https://www.storyblok.com/legal/privacy-policy',
+	],
+	'amazon_web_services' => [
+		'name' => 'Amazon Web Services Inc.',
+		'address' => '410 Terry Avenue North, Seattle, WA 98109-5210, USA',
+		'privacy_policy' => 'https://aws.amazon.com/privacy',
+		'privacy_shield' =>
+			'https://www.privacyshield.gov/participant?id=a2zt0000000TOWQAA4',
+	],
+	'paypal_eu' => [
+		'name' => 'PayPal (Europe) S.a.r.l. et Cie',
+		'address' => 'S.C.A., 22-24 Boulevard Royal, L-2449 Luxembourg, EU',
+		'privacy_policy' => 'https://www.paypal.com/webapps/mpp/ua/privacy-full',
+	],
+	'chargebee' => [
+		'name' => 'Chargebee Inc.',
+		'address' => '340 S Lemon Avenue, #1537, Walnut, California 91789, USA',
+		'privacy_policy' => 'https://www.chargebee.com/privacy/',
 	],
 ];
