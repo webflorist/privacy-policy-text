18fb76c4e2436e2d5106996c6e5569826ed5e871
Gerald Buttinger <gerald@code.florist>
2022-01-03 03:12:51 +0100

    ADD php Helpers class

--- src/php/Helpers.php
+++ src/php/Helpers.php
@@ -4,4 +4,33 @@ namespace Webflorist\PrivacyPolicyText;
 
 class Helpers
 {
+    public static function renderText(string $text)
+    {
+
+        // Render bold text
+        $text = preg_replace_callback(
+            '/\*\*\S[^*]+\S\*\*/',
+            function (array $matches) {
+                return '<strong>' . str_replace('**', '', $matches[0]) . '</strong>';
+            },
+            $text
+        );
+
+        // Render links
+        $text = preg_replace_callback(
+            '/\[\S[^\]]+\]\([^)]+\)/',
+            function (array $matches) {
+                $linkData = explode('](', preg_replace('/[[)]/', '', $matches[0]));
+                return '<a href="' . $linkData[1] . '">' . $linkData[0] . '</a>';
+            },
+            $text
+        );
+
+        return $text;
+    }
+
+    public static function getProcessors()
+    {
+        return require_once dirname(__FILE__) . '/../processors.php';
+    }
 }
